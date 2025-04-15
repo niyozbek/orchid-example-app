32ebaeefea58d11755436d4581ea43bcc881ba56
Niyozbek <imniyozbek@gmail.com>
2025-04-15 11:17:41 +0500

    Implemented simple To-do list.

---
+++ app/Orchid/Screens/TaskScreen.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Orchid\Screens;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Orchid\Screen\Actions\Button;
+use Orchid\Screen\Actions\ModalToggle;
+use Orchid\Screen\Fields\Input;
+use Orchid\Screen\Screen;
+use Orchid\Screen\TD;
+use Orchid\Support\Facades\Layout;
+
+class TaskScreen extends Screen
+{
+    /**
+     * Fetch data to be displayed on the screen.
+     *
+     * @return array
+     */
+    public function query(): iterable
+    {
+        return [
+            'tasks' => Task::latest()->get(),
+        ];
+    }
+
+    /**
+     * The name of the screen displayed in the header.
+     *
+     * @return string|null
+     */
+    public function name(): ?string
+    {
+        return 'Simple To-Do List';
+    }
+
+    /**
+     * The screen's action buttons.
+     *
+     * @return \Orchid\Screen\Action[]
+     */
+    public function commandBar(): iterable
+    {
+        return [
+            ModalToggle::make('Add Task')
+                ->modal('taskModal')
+                ->method('create')
+                ->icon('plus'),
+        ];
+    }
+
+    /**
+     * The screen's layout elements.
+     *
+     * @return \Orchid\Screen\Layout[]|string[]
+     */
+    public function layout(): iterable
+    {
+        return [
+            Layout::table('tasks', [
+                TD::make('id'),
+                TD::make('name'),
+                TD::make('created_at'),
+                TD::make('Actions')
+                    ->alignRight()
+                    ->render(function (Task $task) {
+                        return Button::make('Delete Task')
+                            ->confirm('After deleting, the task will be gone forever.')
+                            ->method('delete', ['task' => $task->id]);
+                    }),
+            ]),
+
+            Layout::modal('taskModal', Layout::rows([
+                Input::make('task.name')
+                    ->title('Name')
+                    ->placeholder('Enter task name')
+                    ->help('The name of the task to be created.'),
+            ]))
+                ->title('Create Task')
+                ->applyButton('Add Task'),
+        ];
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return void
+     */
+    public function create(Request $request)
+    {
+        // Validate form data, save task to database, etc.
+        $request->validate([
+            'task.name' => 'required|max:255',
+        ]);
+
+        $task = new Task();
+        $task->name = $request->input('task.name');
+        $task->save();
+    }
+
+    /**
+     * @param Task $task
+     *
+     * @return void
+     */
+    public function delete(Task $task)
+    {
+        $task->delete();
+    }
+}
--- routes/platform.php
+++ routes/platform.php
@@ -14,6 +14,7 @@ use App\Orchid\Screens\Examples\ExampleTextEditorsScreen;
 use App\Orchid\Screens\PlatformScreen;
 use App\Orchid\Screens\Role\RoleEditScreen;
 use App\Orchid\Screens\Role\RoleListScreen;
+use App\Orchid\Screens\TaskScreen;
 use App\Orchid\Screens\User\UserEditScreen;
 use App\Orchid\Screens\User\UserListScreen;
 use App\Orchid\Screens\User\UserProfileScreen;
@@ -102,3 +103,8 @@ Route::screen('/examples/charts', ExampleChartsScreen::class)->name('platform.ex
 Route::screen('/examples/cards', ExampleCardsScreen::class)->name('platform.example.cards');
 
 // Route::screen('idea', Idea::class, 'platform.screens.idea');
+
+Route::screen('task', TaskScreen::class)->name('platform.task')
+    ->breadcrumbs(function (Trail $trail){
+        return $trail->parent('platform.index')->push('Task');
+    });
